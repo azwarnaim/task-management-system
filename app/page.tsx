47dafa99b408f8345b26585c676b71b4f49c7340
azwarnaim <azwardev@gmail.com>
2026-01-08 14:23:55 +0800

    AddHomeBUtton

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center  font-sans ">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16  sm:items-start">
-      
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center py-32 px-16">
+        <Link href="/list-task">
+          <Button size="lg">Assessment</Button>
+        </Link>
       </main>
     </div>
   );
